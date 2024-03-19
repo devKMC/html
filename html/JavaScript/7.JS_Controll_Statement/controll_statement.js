e2ae8792edc91d9ccf1147a8a4b719121ec0709a
devKMC <allcasa@naver.com>
2024-03-19 11:52:25 +0900

    2024-03-19 11:50

--- html/JavaScript/7.JS_Controll_Statement/controll_statement.js
+++ html/JavaScript/7.JS_Controll_Statement/controll_statement.js
@@ -1,62 +1,114 @@
 /*
     if else 문
-    - 
-    - 
+    - 조건식이 true 일 경우 if 코드 블럭 실행, false 일 경우 else 코드 블럭 실행
+    - 조건식 위치에 논리 값을 반환하는 표현식이 아니더라도 사용 가능
 */
+let age = 10;
+if (age >19) console.log('성인');
+else console.log('미성년자');
 
 
 console.log('==============================');
 
-
+let number = 10;
+let kind = '';
+if (number % 3) kind = '3의 배수가 아님';
+else kind = '3의 배수';
 
 console.log('==============================');
 
 /*
-    - 
+    - if else 문을 삼항 연산자로 변경하여 사용 가능
 */
-
+kind = number % 3 ? '3의 배수가 아님' : '3의 배수';
 
 console.log('==============================');
 
 /*
     switch 문
-    - 
-    - 
-    - 
+    - 표현식을 비교하여 값이 표현식과 일치하면 해당하는 case 문으로 실행 순서를 옮김
+    - 표현식과 일치하는 case 가 존재하지 않으면 default 문으로 이동
+    - break를 지정하지 않으면 맨 끝까지 진행됨
 */
+const weekNumber = 3;
+let weekString = '';
 
+switch(weekNumber){
+    case 0:
+        weekString += '일요일';
+        break;
+    case 1:
+        weekString += '월요일';
+        break;
+    case 2:
+        weekString += '화요일';
+        break;
+    case 3:
+        weekString += '수요일';
+        break;
+    case 4:
+        weekString += '목요일';
+        break;
+    default:
+        weekString += '금토요일';
+}
+console.log(weekString)
 
 
 console.log('==============================');
 
 /*
     for 반복문
-    - 
+    - 반복할 횟수가 지정되 있을 때 사용하는 반복문
 */
+const dan = 3;
+for(let count = 1; count <= 9; count++){
+    console.log(`${dan} * ${count} = ${dan * count}`);
+}
 
 
 console.log('==============================');
 
 /*
     while 반복문
-    - 
+    - 횟수가 지정되어 있지 않을때 사용하는 반복문
 */
+let randomNumber = Math.random();
+console.log(randomNumber);
 
+
+// 0 ~ 1 사이의 임의의값 지정 , 0.5 보다 작으면 탈출 , 그 이상이면 무한 반복
+// 반복문을 먼저 돌리고 조건 검사를 함
+while (randomNumber > 0.5){
+    randomNumber = Math.random();
+    console.log(randomNumber);
+}
 
 console.log('==============================');
 
 /*
     do-while 반복문
-    - 
+    - 반복할 코드 블럭은 한번은 반드시 실행하고 조건을 확인
 */
 
+// 실행코드를 돌리고 조건검사를 함
+let randomNumber2 = 0;
+do{
+    randomNumber2=Math.random();
+    console.log(randomNumber2);
+}while (randomNumber2 > 0.5);
 
 console.log('==============================');
 
 /*
     break 문 
-    - 
+    - switch 문 반복문에서 코드 블럭을 강제로 탈출 할 때 사용
 */
+let count =0;
+while (true){
+    if (count ===5) break;
+    console.log(count ++);
+}
 
 
 
@@ -64,7 +116,15 @@ console.log('==============================');
 
 /*
     continue 문
-    - 
+    - 반복문의 진행 위치를 맨 마지막으로 이동  (반복문 중괄호 끝까지)
 */
-
+count = 0;
+while (true){
+    if(count === 10) break;
+    if(count % 2 === 0){
+        count++;
+        continue;
+    }
+    console.log(count++);
+}
 
