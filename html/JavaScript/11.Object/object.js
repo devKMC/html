92cab44a88c160980108e7e04846d5565cdb3132
devKMC <allcasa@naver.com>
2024-03-21 10:51:26 +0900

    2024-03-21

--- html/JavaScript/11.Object/object.js
+++ html/JavaScript/11.Object/object.js
@@ -78,39 +78,79 @@ console.log('==============================');
 
 /*
     속성 값 읽기 : 
-    - 
-    - 
-    - 
-    - 
+    - . 표기법과 [] 표기법으로 접근 가능
+    - . 표기법은 유효한 이름일 때 사용
+    - [] 표기법은 유효한 이름이 아닐 때와 접근할 속성이 동적으로 변할 때 ([] 표기법 사용시 반드시 문자열로 지정)
+    - 객체에 존재하지 않는 속성에 접근하면 undefined를 반환
 */
-
-
+console.log(kimminchul.age);
+// console.log(kimminchul.first name);
+console.log(kimminchul['first name']);
+let property = 'first name';
+console.log(kimminchul [property]);
 
 console.log('==============================');
 
 /*
     속성 값 변경 :
-    - 
+    - 객체속성에 접근하여 새로운 값을 할당하여 변경 가능 
+    - 만약 존재하지 않는 속성에 값을 할당하면 새로운 속성이 생성됨
 */
-
-
+goJong.name = '이형';
+goJong.birth = '1852년 9월 8일'
+console.log(goJong)
 
 console.log('==============================');
 
 /*
-    속성 삭제 :
-    - 
+    속성 삭제 : 
+    - delete 연산자를 사용하여 객체 속성 제거 가능
 */
-
+delete goJong.birth;
+console.log(goJong);
 
 console.log('==============================');
 
 /*
     for-in 문 : 
-    - 
-    - 
+    - for - in 문으로 객체에 포함되어 있는 키를 순회해서 접근 가능
+    - 반환되는 값은 문자열 타입의 키가 반환
 */
-
+for (const key in goJong){
+    console.log(`${key} : ${goJong[key]}`)
+}
 
 
 console.log('==============================');
+
+// 주소를 통해 참조값을 가르킴으로 변경이 됨
+let soonJong = goJong;
+soonJong.name = '이척';
+
+console.log(soonJong.name);
+console.log(goJong.name);
+
+soonJong ={
+    name : goJong.name,
+    age : goJong.age,
+    address : goJong.address
+}
+
+soonJong ={};
+for (const key in goJong){
+    soonJong[key] = goJong[key];
+}
+console.log(soonJong);
+
+soonJong = {};
+console.log(soonJong);
+
+// 고종에 있는 값을 그대로 복사해서 넣어줌  (스레드 연산자)
+// name , age , address
+soonJong = {...goJong}; // 새로운 주소를 가지는 공간을 생성 (포인트배열 같은 느낌)
+console.log(soonJong);
+
+goJong.name = '이형';
+console.log(goJong.name);
+console.log(soonJong.name);
+
