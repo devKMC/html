98fba5a60862dc4d88572560c79e1cf45af36c6f
devKMC <allcasa@naver.com>
2024-03-26 11:50:50 +0900

    2024-03-26 11:50

--- estate/src/sign-in/script.js
+++ estate/src/sign-in/script.js
@@ -6,10 +6,12 @@ function onSignInButtonClickHandler(event){
     const id=document.getElementById('id').value;
     const password = document.getElementById('password').value;
 
+    const signInMessageElement = document.getElementById('sign-in-message');
     if (id === ID && password === PASSWORD){
         alert('로그인성공');
+        signInMessageElement.textContent = '';
     }else {
-        const signInMessageElement = document.getElementById('sign-in-message');
+    
         signInMessageElement.textContent = '로그인 정보가 일치하지 않습니다.';
         
     }
