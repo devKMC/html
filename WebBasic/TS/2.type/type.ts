8912e526a5db87ac5d58fcb9bccdb53d58978b33
devKMC <allcasa@naver.com>
2024-04-02 10:51:59 +0900

    2024-04-02 10:50

--- WebBasic/TS/2.type/type.ts
+++ WebBasic/TS/2.type/type.ts
@@ -13,48 +13,74 @@ function func(arg: number): string {
     return arg + '';
 }
 
-const arrowFunction = (arg: number): String => {
+const arrowFunction = (arg: number): string => {
     return arg + '';
 }
 
 /*
     typescript의 타입
-    - 
-    -
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
+    - string: 문자열
+    - number: 숫자
+    - boolean: 논리
+    - null: null 값
+    - undefined: 정의되지 않음
+    - object: 참조 타입
+    - array: 배열 타입
+    - enum: 열거형 타입
+    - void: 함수에서 반환값이 없음
+    - any: 모든 타입
 */
+const numbers: number[] = [1, 2, 3, 4, 5] // 더 많이 사용하는 표현
+const numbers2: Array<number> = [1, 2, 3, 4, 5]; // array 라는 클래스를 사용하기
 
+
+enum Color {
+    RED, GREEN, BLUE
+}
+const color: Color = Color.RED;
+
+// any = object랑 같은 표현
+let anyV: any = 10;
+anyV = '10';
+
+const aFun = (): void => {
+
+}
 
 /*
     타입추론 :
-    - 
+    - 타입을 선언하지 않고 변수에 값이 할당되는 순간에 할당된 값에 따라 변수의 타입이 결정나는 것 
 */
 
-
+let stringValuable = '문자열';
+// stringValuable = 123;
 
 /*
     타입 변환 :
-    - 
+    - as 키워드로 형변환 가능 (객체일 때 자주 사용)
 */
 
-
+const PI: any = '3.14';
+let piNumber: number = PI as number; // 넘버타입으로 지정
+piNumber = Number(PI);
 
 /*
     유니온 타입
-    - 
+    - 복수 타입을 지정하는 방법 | (ex: number 타입과 null 타입을 동시에 지정)
 */
 
-
+let union: number | null | string;
+union = 10;
+union = null;
+union = 'null';
 
 /*
     리터럴 타입
-    - 
+    - 지정한 리터럴 값만 가질 수 있는 타입 
 */
+
+let gender: '남' | '여';
+gender = '남'
+gender = '여'
+// gender = '남자'
 
--- WebBasic/TS/3.class/class.ts
+++ WebBasic/TS/3.class/class.ts
@@ -1,49 +1,105 @@
 /*
     클래스 정의 :
-    - 
+    - 속성을 사전 정의해서 사용
 */
+class Sample1 {
+    field: string;
 
+    constructor(field: string) {
+        this.field = field;
+    }
+}
 
 
 console.log('==================================================');
 
 /*
     접근제어자
-    - 
-    - 
-    - 
-    - 
+    - public: 클래스 내부, 자식 클래스 내부, 클래스 인스턴스 
+    - protected: 클래스 내부, 자식 클래스 내부
+    - private: 클래스 내부
+    - typescript에는 패키지라는 개념이 없기 때문에 기본적으로 public으로 지정됨
 */
 
+class Sample2 {
+    private privateField: string;
+    protected protectedField: string;
+    public publicField: string;
+
+    constructor() {
+        this.privateField = '프라이빗 필드';
+        this.protectedField = '프로텍티드 필드';
+        this.publicField = '퍼블릭 필드';
+    }
+}
 
 
 console.log('==================================================');
 
 /*
     생성자 매개변수에 접근 제어자 지정
-    - 
+    - 생성자의 매개변수에 접근 제어자를 지정하여 필드 선언 가능
 */
+class Sample3 {
 
 
+    constructor(
+        public publicField: string, // 초기화가 따로 필요없는 작업들
+        protected protectedField: string,
+        private privateField: string,
+        localVariable: string  // 속성필드가 아닌 매개변수로만 지정이 됨
+    ) {
+        this.publicField = publicField;
+        // this.localVariable = localVariable; 필드가 아니고 매개변수이기에 생성이 되지 않음.
+    }
+
+}
+
+const sample3Instance = new Sample3('publicField', 'protectedField', 'privateField', 'localVariable');
+console.log(sample3Instance);
 
 console.log('==================================================');
 
 /*
     readonly :
-    -  
-    - 
-    - 
+    - readonly가 선언된 클래스 속성은 선언 시 또는 생성자 내부에서만 결정
+    - 값을 재할당 할 수 없고 오직 읽기만 가능
+    - 상수 선언에 사용됨
 */
 
+class Sample4 {
+    public readonly field: string = '퍼블릭 읽기 전용 필드1';
+    public readonly field2: string;
 
+    // 생성자
+    constructor(
+        field2: string,
+        public readonly field3: string
+    ) {
+        this.field2 = field2;
+    }
+}
+
+const sample4Instance = new Sample4('퍼블릭 읽기 전용 필드2', '퍼블릭 읽기전용 필드3');
+console.log(sample4Instance);
+// sample4.sample4Instance.field1= '변경'; // 읽기 전용이기에 값을 변경하진 못함
 
 console.log('==================================================');
 
 /*
     추상 클래스 :
-    - 
+    - 하나 이상의 추상 메서드를 포함하는 클래스, abstaract 키워드로 선언
 */
 
+abstract class AbstaractClass {
+    abstract abstractMethod(arg: string): string;
+}
 
+class SubClass extends AbstaractClass{
+
+    abstractMethod(arg: string): string {
+        return arg;
+    }
+}
 
 console.log('==================================================');
